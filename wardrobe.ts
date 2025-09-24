0bf4069d7093316b05b41f59e07f84e60dfc23b6
Raghav <120128871+Raghav-Luthra@users.noreply.github.com>
2025-09-24 20:32:28 +0530

    Update wardrobe.ts

--- wardrobe.ts
+++ wardrobe.ts
@@ -7,21 +7,7 @@ import { WardrobeItem } from './types';
 
 // Default wardrobe items hosted for easy access
 export const defaultWardrobe: WardrobeItem[] = [
-  {
-    id: 'gemini-sweat',
-    name: 'Gemini Sweat',
-    url: 'https://raw.githubusercontent.com/ammaarreshi/app-images/refs/heads/main/gemini-sweat-2.png',
-  },
-  {
-    id: 'gemini-tee',
-    name: 'Gemini Tee',
-    url: 'https://raw.githubusercontent.com/ammaarreshi/app-images/refs/heads/main/Gemini-tee.png',
-  },
-  {
-    id: 'white-sherwani',
-    name: 'white Sherwani',
-    url: 'https://raw.githubusercontent.com/Raghav-Luthra/Spannick-Designers-Visual/main/whitesherwani.png',
-  },
+  
   {
     id: 'black-jodhpuri',
     name: 'Black Jodhpuri Suit',
